8ac2521d746b66c9713b080f836578a5f70cae32
Jack Vento <jackovento@gmail.com>
2020-04-16 17:51:24 -0700

    Root Motion testing

--- Source/TrailblazerCrisis/Public/Player/PlayerCharacter.h
+++ Source/TrailblazerCrisis/Public/Player/PlayerCharacter.h
@@ -60,22 +60,26 @@ public:
 protected:
 
 	/** Called for forwards/backward input */
-	void MoveForward(float Value);
+	UFUNCTION(BlueprintCallable, Category = Movement)
+		void MoveForward(float Value);
 
 	/** Called for side to side input */
-	void MoveRight(float Value);
+	UFUNCTION(BlueprintCallable, Category = Movement)
+		void MoveRight(float Value);
 
 	/**
 	 * Called via input to turn at a given rate.
 	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
 	 */
-	void TurnAtRate(float Rate);
+	UFUNCTION(BlueprintCallable, Category = Movement)
+		void TurnAtRate(float Rate);
 
 	/**
 	 * Called via input to turn look up/down at a given rate.
 	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
 	 */
-	void LookUpAtRate(float Rate);
+	UFUNCTION(BlueprintCallable, Category = Movement)
+		void LookUpAtRate(float Rate);
 
 	// APawn interface
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
